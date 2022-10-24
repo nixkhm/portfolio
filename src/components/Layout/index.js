1a9173ef2a3de7a0cc33da1b88efdd41b4682de7
Nicholas Masters <nicholas.masters20@stjohns.edu>
2022-10-23 20:15:10 -0400

    favicon fix and readme

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,27 +1,24 @@
-import SideBar from '../SideBar';
-import './index.scss';
-import {Outlet} from 'react-router-dom'
+import SideBar from '../SideBar'
+import './index.scss'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-    return (
-        <div className = "App">
-            <SideBar />
-            <div className = 'page'>
-                <span className= 'tags top-tags'>&lt;body&gt;</span>
+  return (
+    <div className="App">
+      <SideBar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
 
-                <Outlet />
+        <Outlet />
 
-                <span className = 'tags bottom-tags'>&lt;/body&gt;
-                
-                < br />
-                
-                <span className = 'bottom-tag-html'>&lt;/html&gt;</span>
-
-                </span>
-
-            </div>
-        </div>
-    )
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
